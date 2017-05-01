907ae8b1f680a9e0925454567442b0e0d1348dac
awesson <ajwesson0@gmail.com>
2017-04-30 23:41:04 -0400

    Adding C# client
    Setup a tcp client in C# to grab any new state from the frontend (and
    eventually save it off and output new code).

---
+++ backend/HieroglyphBackend/ByteArrayExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace HieroglyphBackend
+{
+	public static class ByteArrayExtension
+	{
+		public static int FindSequence(this byte[] bytes, byte[] sequence)
+		{
+			return FindSequence(bytes, 0, bytes.Length, sequence);
+		}
+
+		public static int FindSequence(this byte[] bytes, int startIndex, int count, byte[] sequence)
+		{
+			var endIndex = Math.Min(startIndex + count, bytes.Length);
+			for (var mainIndex = startIndex; mainIndex < endIndex; ++mainIndex)
+			{
+				var foundMatch = true;
+				for (var seqIndex = 0; seqIndex < sequence.Length; ++seqIndex)
+				{
+					if (bytes[mainIndex + seqIndex] != sequence[seqIndex])
+					{
+						foundMatch = false;
+						break;
+					}
+				}
+
+				if (foundMatch)
+				{
+					return mainIndex;
+				}
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ backend/HieroglyphBackend/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HieroglyphBackend
+{
+	static class Program
+	{
+		public static void Main(string[] args)
+		{
+			// TODO(awesson): Startup the front-end
+			
+			FrontEndListener.Start();
+
+			// Block and wait for a new state, and then process it
+			// When there are no more states (because the stream closed) the app will close.
+			foreach (var state in FrontEndListener.RecieveStates())
+			{
+				ExportState(state);
+			}
+		}
+
+		private static void ExportState(string state)
+		{
+			Console.WriteLine("Got new state:\n{0}", state);
+			// TODO(awesson): Save state to disk for starting up the front-end next time.
+
+			// TODO(awesson): Parse json state into code and save code to disk.
+		}
+	}
+}
