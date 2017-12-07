8a6f49b18f2c8c9f325a284d5e37edb33f49c1fe
awesson <ajwesson0@gmail.com>
2017-12-06 23:54:22 -0500

    Setup saving and loading the state
    Added FileOps.ts which has the save and load state functions. Currently
    auto saves as store.json to the user directory (AppData/heiroglyph) at
    most once a second after any changes are made.

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import throttle from 'lodash/throttle';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, Store } from 'redux';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.css';
-//import throttle from 'lodash/throttle';
 
-import { initRootState, RootState } from './RootState';
-import rootReducer from './RootReducer';
 import App from './App';
+import FileOps from './FileOps/FileOps';
+import rootReducer from './RootReducer';
+import { initRootState, RootState } from './RootState';
 
+
+const autoSaveIntervalMS = 1000;
 
 const castWindow = window as any;
-const store = createStore(rootReducer,
-                          initRootState(),
-                          // Needed for the redux firefox plugin to work
-                          castWindow.devToolsExtension && castWindow.devToolsExtension());
+const store = createStore(
+	rootReducer,
+	FileOps.loadStore(),
+	// Needed for the redux firefox plugin to work
+	castWindow.devToolsExtension && castWindow.devToolsExtension());
 
-//	store.subscribe(throttle(getSaveFunc(store), 1000));
+function saveStore()
+{
+	FileOps.saveStore(store);
+}
+store.subscribe(throttle(saveStore, autoSaveIntervalMS));
 
 ReactDOM.render(
 	<Provider store={store}>
